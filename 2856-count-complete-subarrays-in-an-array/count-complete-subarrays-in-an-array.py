9ccd5c4f4285ede65c64b5ca8fc9ab8df3b4e4a7
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-04-24 09:01:01 +0300

    Time: 23 ms (38.46%) | Memory: 18.1 MB (30.24%) - LeetSync

--- 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.py
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.py
@@ -1,17 +1,25 @@
 class Solution:
-    def countCompleteSubarrays(self, nums):
-        unique_count = len(set(nums))
-        elem_freq = Counter()
-        total_subarrays, length = 0, len(nums)
-        start_index = 0
-      
-        for end_index, value in enumerate(nums):
-            elem_freq[value] += 1
-            while len(elem_freq) == unique_count:
-                total_subarrays += length - end_index
-                elem_freq[nums[start_index]] -= 1
-                if elem_freq[nums[start_index]] == 0:
-                    elem_freq.pop(nums[start_index])
-                start_index += 1
-      
-        return total_subarrays
+    def countCompleteSubarrays(self, nums: List[int]) -> int:
+        looking = Counter(set(nums))
+        window = Counter()
+        need = len(looking)
+        N = len(nums)
+        l, r = 0,0
+        ans = 0
+        while l <=r and r <=N-1:
+            if nums[r] not in window and nums[r] in looking:
+                need -=1
+            window[nums[r]] +=1
+            while need  == 0 and l <= r and l <=N-1:
+                ans +=(N-r)
+                if nums[l] in looking:
+                    window[nums[l]] -=1
+                    if window[nums[l]] == 0:
+                        del window[nums[l]] 
+                        need +=1
+                l +=1
+            r +=1
+        return ans
+
+
+            
