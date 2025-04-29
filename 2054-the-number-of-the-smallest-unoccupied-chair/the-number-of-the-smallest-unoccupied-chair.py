59cecde2103887ef913c11afd7f1d52279202fde
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-04-29 16:29:10 +0300

    Time: 4094 ms (6.62%) | Memory: 21.5 MB (96.52%) - LeetSync

--- 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.py
+++ 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.py
@@ -1,18 +1,16 @@
-from collections import *
-
 class Solution:
     def smallestChair(self, times: List[List[int]], targetFriend: int) -> int:
-        events = [(t[0], f) for f,t in enumerate(times)] + [(t[1], -f) for f,t in enumerate(times)]
-        events.sort()
-        available_seats = list(range(len(times)))
-        seat = dict()
-        for t, f in events:
-            f = abs(f)
-            if f == targetFriend:
-                return heappop(available_seats)
-            if f in seat:
-                heappush(available_seats, seat[f])
-                del seat[f]
-            else:
-                seat[f] = heappop(available_seats) 
-        
+        target_time = times[targetFriend]
+        times.sort()
+
+        n = len(times)
+        chair_time = [0] * n
+
+        for time in times:
+            for i in range(n):
+                if chair_time[i] <= time[0]:
+                    chair_time[i] = time[1]
+                    if time == target_time:
+                        return i
+                    break
+        return 0
