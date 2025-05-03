e7cb384aa0814a977e7aaf376a5b2da44ab1f78a
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-05-03 17:16:05 +0300

    Time: 4 ms (37.04%) | Memory: 20.1 MB (37.80%) - LeetSync

--- 419-battleships-in-a-board/battleships-in-a-board.py
+++ 419-battleships-in-a-board/battleships-in-a-board.py
@@ -1,3 +1,5 @@
+
+
 class Solution:
     def countBattleships(self, board: List[List[str]]) -> int:
         R, C = len(board), len(board[0])
@@ -5,17 +7,19 @@ class Solution:
         ans  = 0
         def inbound(r, c):
             return 0 <=r<R and 0 <=c<C
-        def dfs(r, c):
-            board[r][c] = "#"
-            for x, y in directions:
-                nr, nc = x + r, y + c
-                if inbound(nr, nc) and board[nr][nc] == "X":
-                    dfs(nr, nc)
 
         for i in range(R):
             for j in range(C):
                 if board[i][j] == "X":
-                    dfs(i, j)
-                    ans +=1
+                    # top
+                    tr, tc = i-1, j
+                    # row
+                    rr, rc = i,  j-1
+                    top =  inbound(tr, tc) and board[tr][tc] == "X"
+                    row = inbound(rr, rc) and board[rr][rc] == "X"
+                    if not (top or row):
+                        ans +=1
+
+
         return ans
 
