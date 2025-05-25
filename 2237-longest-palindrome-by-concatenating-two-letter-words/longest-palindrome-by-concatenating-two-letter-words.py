349e64792054e65542407ff0cd7f6656ef72305a
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-05-25 10:35:34 +0300

    Time: 47 ms (76.90%) | Memory: 35.7 MB (12.27%) - LeetSync

--- 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.py
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.py
@@ -7,7 +7,7 @@ class Solution:
             rev  = key[::-1]
             if rev == key and rep_count[key] % 2 ==1:
                 similar = True
-                cnt +=(min(rep_count[rev]-1, rep_count[key]-1))
+                cnt +=min(rep_count[rev]-1, rep_count[key]-1) * 2
             else:
-                cnt +=(min(rep_count[rev], rep_count[key]))
-        return cnt * 2 + (2 if similar else 0)
+                cnt +=(min(rep_count[rev], rep_count[key])*2)
+        return cnt  + (2 if similar else 0)
