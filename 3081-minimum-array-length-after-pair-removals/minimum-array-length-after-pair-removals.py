e0a989092482ee6080730c785218b1d4f8ea9ccc
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-05-27 09:17:41 +0300

    Time: 0 ms (100.00%) | Memory: 27.7 MB (76.68%) - LeetSync

--- 3081-minimum-array-length-after-pair-removals/minimum-array-length-after-pair-removals.py
+++ 3081-minimum-array-length-after-pair-removals/minimum-array-length-after-pair-removals.py
@@ -1,19 +1,6 @@
 class Solution:
     def minLengthAfterRemovals(self, nums: List[int]) -> int:
-        heap = []
-        f = Counter(nums)
-        for key, value in f.items():
-            heappush(heap, (-value, key))
-        while heap:
-            value, key = heappop(heap)
-            if not heap:
-                return abs(value)
-            val1, key1 = heappop(heap)
-            value +=1
-            val1 +=1
-            if val1:
-                heappush(heap, (val1, key1))
-            if value:
-                heappush(heap, (value, key))
-        return 0
-
+        n = len(nums)
+        i = nums[n // 2]
+        cnt = bisect_right(nums, i) - bisect_left(nums, i)
+        return max(n % 2, 2 * cnt - n)
