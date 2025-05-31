bd2317c0db820440e26e2219093c7412a01ff78e
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-05-31 15:15:38 +0300

    Time: 51 ms (39.64%) | Memory: 19.3 MB (5.21%) - LeetSync

--- 97-interleaving-string/interleaving-string.py
+++ 97-interleaving-string/interleaving-string.py
@@ -1,22 +1,16 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        if len(s1) + len(s2) !=len(s3):
+        if len(s1) + len(s2) != len(s3):
             return False
         @cache
-        def dp(i, j, index):
-            if len(s1) == i:
-                return s3.endswith(s2[j:])
-            if len(s2) == j:
-                return s3.endswith(s1[i:])
-            if index >= len(s3):
-                return False
-            if s1[i] == s2[j]== s3[index]:
-                return dp(i+1, j, index+1) or dp(i, j+1, index+1)
-            elif  s1[i]== s3[index]:
-                return dp(i+1, j, index+1)
-            elif s2[j] == s3[index]:
-                return dp(i, j+1, index+1)
-            else:
-                return False
-        return dp(0, 0, 0)
-
+        def dp(i, j):
+            k = i + j  
+            if k == len(s3):
+                return True
+            ans = False
+            if i < len(s1) and s1[i] == s3[k]:
+                ans |= dp(i + 1, j)
+            if j < len(s2) and s2[j] == s3[k]:
+                ans |= dp(i, j + 1)
+            return ans
+        return dp(0, 0)
