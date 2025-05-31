3bd22b77854080a5bd2f9a3f2a3e8baaac3c34be
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-05-31 15:16:52 +0300

    Time: 49 ms (50.88%) | Memory: 19.3 MB (5.21%) - LeetSync

--- 97-interleaving-string/interleaving-string.py
+++ 97-interleaving-string/interleaving-string.py
@@ -2,9 +2,11 @@ class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
         if len(s1) + len(s2) != len(s3):
             return False
-        @cache
+        memo = {}
         def dp(i, j):
             k = i + j  
+            if (i, j ) in memo:
+                return memo[(i, j)]
             if k == len(s3):
                 return True
             ans = False
@@ -12,5 +14,6 @@ class Solution:
                 ans |= dp(i + 1, j)
             if j < len(s2) and s2[j] == s3[k]:
                 ans |= dp(i, j + 1)
+            memo[(i, j)] = ans
             return ans
         return dp(0, 0)
