0b92e3a1b8a43ad3d01acdea02f82fd8ffbfb539
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-06-06 09:11:53 +0300

    Time: 431 ms (67.17%) | Memory: 21.8 MB (29.01%) - LeetSync

---
+++ 2520-using-a-robot-to-print-the-lexicographically-smallest-string/using-a-robot-to-print-the-lexicographically-smallest-string.py
@@ -0,0 +1,21 @@
+class Solution:
+    def robotWithString(self, s: str) -> str:
+        left = [""] * len(s)
+        stack = []
+        N = len(s)
+        for i in range(len(s)-1, -1, -1):
+            if i == N-1:
+                left[i] = s[i]
+            else:
+                left[i] = min(left[i+1], s[i])
+        ans = []
+        for i in range(N):
+            while stack and stack[-1] <=left[i]:
+                    ans.append(stack.pop())
+            if left[i] != s[i]:
+                stack.append(s[i])
+            else:
+                ans.append(s[i])
+        while stack:
+            ans.append(stack.pop())
+        return "".join(ans)
