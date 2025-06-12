50194a83b96539e0b8b8a879a63cd87260976e5e
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-06-12 17:45:10 +0300

    Add leetcode: 2460. Apply Operations to an Array

---
+++ leetcode/easy/2460_apply_operations_to_an_array.cpp
@@ -0,0 +1,11 @@
+/*
+ * 2460. Apply Operations to an Array
+ * Platform: LEETCODE
+ * Difficulty: easy
+ * Language: cpp
+ * Problem URL: https://leetcode.com/problems/apply-operations-to-an-array/submissions/1559264790/
+ * Date: 2025-06-12T14:45:11.547Z
+ * Author: CodeSync Extension
+ */
+
+c
