c6d2851bbba341556e5d94283683e35c3861ca61
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-06-12 18:50:40 +0300

    Add leetcode: 867. Transpose Matrix

---
+++ leetcode/easy/867_transpose_matrix.cpp
@@ -0,0 +1,11 @@
+/*
+ * 867. Transpose Matrix
+ * Platform: LEETCODE
+ * Difficulty: easy
+ * Language: cpp
+ * Problem URL: https://leetcode.com/problems/transpose-matrix/description/
+ * Date: 2025-06-12T15:50:40.643Z
+ * Author: CodeSync Extension
+ */
+
+c
