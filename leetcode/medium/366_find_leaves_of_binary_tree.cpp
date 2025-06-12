9a5b1c793188e64037290758c336480b10696530
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-06-12 21:34:07 +0300

    Add leetcode: 366. Find Leaves of Binary Tree

---
+++ leetcode/medium/366_find_leaves_of_binary_tree.cpp
@@ -0,0 +1,11 @@
+/*
+ * 366. Find Leaves of Binary Tree
+ * Platform: LEETCODE
+ * Difficulty: medium
+ * Language: cpp
+ * Problem URL: https://leetcode.com/problems/find-leaves-of-binary-tree/submissions/842448910/
+ * Date: 2025-06-12T18:34:08.696Z
+ * Author: CodeSync Extension
+ */
+
+#
