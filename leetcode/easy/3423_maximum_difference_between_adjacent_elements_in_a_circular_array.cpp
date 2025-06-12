4238187132721835a7afa89eee1661aaa3732478
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-06-12 09:32:30 +0300

    Add leetcode: 3423. Maximum Difference Between Adjacent Elements in a Circular Array

---
+++ leetcode/easy/3423_maximum_difference_between_adjacent_elements_in_a_circular_array.cpp
@@ -0,0 +1,11 @@
+/*
+ * 3423. Maximum Difference Between Adjacent Elements in a Circular Array
+ * Platform: LEETCODE
+ * Difficulty: easy
+ * Language: cpp
+ * Problem URL: https://leetcode.com/problems/maximum-difference-between-adjacent-elements-in-a-circular-array/submissions/1512700934/
+ * Date: 2025-06-12T06:32:09.566Z
+ * Author: CodeSync Extension
+ */
+
+c
