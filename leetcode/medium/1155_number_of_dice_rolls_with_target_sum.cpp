7b293943656443ea1ad7b96d0a17e0ba243d01dc
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-06-14 01:09:19 +0300

    Add leetcode: 1155. Number of Dice Rolls With Target Sum

---
+++ leetcode/medium/1155_number_of_dice_rolls_with_target_sum.cpp
@@ -0,0 +1,11 @@
+/*
+ * 1155. Number of Dice Rolls With Target Sum
+ * Platform: LEETCODE
+ * Difficulty: medium
+ * Language: cpp
+ * Problem URL: https://leetcode.com/problems/number-of-dice-rolls-with-target-sum/submissions/1128595516/
+ * Date: 2025-06-13T22:09:21.900Z
+ * Author: CodeSync Extension
+ */
+
+c
