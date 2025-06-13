cd92d8287a2007d7fcd1f90c5009ded98541133f
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-06-13 21:18:17 +0300

    Add leetcode: 299. Bulls and Cows

---
+++ leetcode/medium/299_bulls_and_cows.cpp
@@ -0,0 +1,11 @@
+/*
+ * 299. Bulls and Cows
+ * Platform: LEETCODE
+ * Difficulty: medium
+ * Language: cpp
+ * Problem URL: https://leetcode.com/problems/bulls-and-cows/submissions/1663212403/
+ * Date: 2025-06-13T18:18:19.751Z
+ * Author: CodeSync Extension
+ */
+
+c
