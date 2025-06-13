ab1559495f7936f1107eab98dd52b8b44deac774
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-06-13 22:09:10 +0300

    Add leetcode: 26. Remove Duplicates from Sorted Array

---
+++ leetcode/easy/26_remove_duplicates_from_sorted_array.cpp
@@ -0,0 +1,11 @@
+/*
+ * 26. Remove Duplicates from Sorted Array
+ * Platform: LEETCODE
+ * Difficulty: easy
+ * Language: cpp
+ * Problem URL: https://leetcode.com/problems/remove-duplicates-from-sorted-array/submissions/1629837962/
+ * Date: 2025-06-13T19:09:12.415Z
+ * Author: CodeSync Extension
+ */
+
+c
