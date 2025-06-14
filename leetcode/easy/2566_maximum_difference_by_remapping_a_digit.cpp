a561adf3691dad606f3618d2469db5bed9686797
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-06-14 09:47:57 +0300

    Add leetcode: 2566. Maximum Difference by Remapping a Digit

---
+++ leetcode/easy/2566_maximum_difference_by_remapping_a_digit.cpp
@@ -0,0 +1,11 @@
+/*
+ * 2566. Maximum Difference by Remapping a Digit
+ * Platform: LEETCODE
+ * Difficulty: easy
+ * Language: cpp
+ * Problem URL: https://leetcode.com/problems/maximum-difference-by-remapping-a-digit/
+ * Date: 2025-06-14T06:48:00.651Z
+ * Author: CodeSync Extension
+ */
+
+c
